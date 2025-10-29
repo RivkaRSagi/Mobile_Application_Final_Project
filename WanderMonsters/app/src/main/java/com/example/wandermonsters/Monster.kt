30d9f66adedbf140c6910fa72a7622b45dd5989f
RivkaRSagi <rivka.r.sagi@gmail.com>
2025-10-28 21:41:53 -0400

    WanderMonster initial files

---
+++ WanderMonsters/app/src/main/java/com/example/wandermonsters/Monster.kt
@@ -0,0 +1,11 @@
+package com.example.wandermonsters
+
+class Monster {
+    var type = String
+    var petName = String
+    var size = Float
+    var weight = Float
+    var intellect = Int
+    var hobby = String
+    var rarity = Int
+}
