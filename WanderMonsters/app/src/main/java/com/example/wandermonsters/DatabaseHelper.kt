b3db317fdff480e12fe436328439bcb01d987d46
RivkaRSagi <rivka.r.sagi@gmail.com>
2025-11-02 02:31:36 -0500

    monster type names, started database file

---
+++ WanderMonsters/app/src/main/java/com/example/wandermonsters/DatabaseHelper.kt
@@ -0,0 +1,8 @@
+package com.example.wandermonsters
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper {
+}
