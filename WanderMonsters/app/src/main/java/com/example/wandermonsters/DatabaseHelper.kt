a18fd2963fc511bf891e6d0cf62a9194e87a224d
RivkaRSagi <rivka.r.sagi@gmail.com>
2025-11-13 20:17:47 -0500

    added database and monster functions

--- WanderMonsters/app/src/main/java/com/example/wandermonsters/DatabaseHelper.kt
+++ WanderMonsters/app/src/main/java/com/example/wandermonsters/DatabaseHelper.kt
@@ -33,6 +33,7 @@ class DatabaseHelper (context: Context): SQLiteOpenHelper(
             """
                 CREATE TABLE IF NOT EXISTS PetMonsters (
                 Owner_ID INTEGER NOT NULL,
+                Monster_ID INTEGER PRIMARY KEY AUTOINCREMENT,
                 Name TEXT NOT NULL,
                 Type INTEGER NOT NULL,
                 Size FLOAT NOT NULL,
@@ -50,6 +51,213 @@ class DatabaseHelper (context: Context): SQLiteOpenHelper(
         db.execSQL("DROP TABLE IF EXISTS MonsterDefinitions")
         db.execSQL("DROP TABLE IF EXISTS PetMonsters")
         onCreate(db)
+    }
+
+    //Accounts functions:
+
+
+    //function to create a new account
+    fun newUser(username: String, password: String){
+        val db = writableDatabase
+        db.execSQL("INSERT INTO Accounts (username, password) VALUES (?,?)", arrayOf(username, password))
+    }
+
+    //function to delete user account
+    fun deleteAccount(ID: Int){
+        val db = writableDatabase
+        db.execSQL("DELETE FROM Accounts WHERE id = ?", arrayOf(ID))
+    }
+
+    //function to retrieve owner id based on username and password
+    fun getOwnerID(username: String, password: String): Int {
+        val db = readableDatabase
+        var ownerID = 0
+        val cursor = db.rawQuery(
+            "SELECT id FROM Accounts WHERE username = ? AND password = ?",
+            arrayOf(username, password)
+        )
+
+        if (cursor.moveToFirst()) {
+            ownerID = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
+        }
+        cursor.close()
+        return ownerID
+    }
+
+
+
+    //PetMonsters functions:
+
+    //function to add monster
+    fun addPetMonster(ownerID: Int, thisMonster: Monster){
+        val db = writableDatabase
+        db.execSQL("INSERT INTO PetMonsters (Owner_ID, Name, Type, Size, Weight, Intellect, Hobby, Rarity) VALUES (?,?,?,?,?,?,?,?)",
+            arrayOf(ownerID, thisMonster.petName, thisMonster.type, thisMonster.size, thisMonster.weight, thisMonster.intellect, thisMonster.hobby, thisMonster.rarity))
+
+        db.execSQL("UPDATE MonsterDefinitions SET Discovered = 1 WHERE Type_Name = ?", arrayOf(thisMonster.type))
+    }
+
+    //function to rename monster
+    fun renameMonster(petID: Int, newName: String) {
+        val db = writableDatabase
+        db.execSQL("UPDATE PetMonsters SET Name = ? WHERE Monster_ID = ?", arrayOf(newName, petID))
+    }
+
+    //function to retrieve a single monster based on id
+    fun getPetMonster(petID: Int): Monster {
+        val db = readableDatabase
+        val cursor = db.rawQuery(
+            "SELECT Name, Type, Size, Weight, Intellect, Hobby, Rarity FROM PetMonsters WHERE Monster_ID = ?",
+            arrayOf(petID.toString())
+        )
+        var monster = Monster("", "", 0f, 0f, 0, "", 0)
+
+        if (cursor.moveToFirst()) {
+            monster = Monster(
+                cursor.getString(cursor.getColumnIndexOrThrow("Type")),
+                cursor.getString(cursor.getColumnIndexOrThrow("Name")),
+                cursor.getFloat(cursor.getColumnIndexOrThrow("Size")),
+                cursor.getFloat(cursor.getColumnIndexOrThrow("Weight")),
+                cursor.getInt(cursor.getColumnIndexOrThrow("Intellect")),
+                cursor.getString(cursor.getColumnIndexOrThrow("Hobby")),
+                cursor.getInt(cursor.getColumnIndexOrThrow("Rarity"))
+            )
+        }
+        cursor.close()
+        return monster
+    }
+
+    //function to get monster id based on name, owner, and size
+    fun getPetID(ownerID: Int, petName: String, petSize: Float): Int {
+        val db = readableDatabase
+        var petID = 0
+        val cursor = db.rawQuery("SELECT Monster_ID FROM PetMonsters WHERE Owner_ID=? AND Name=? AND Size=?",
+            arrayOf(ownerID.toString(), petName, petSize.toString()))
+        if (cursor.moveToFirst()) {
+            petID = cursor.getInt(cursor.getColumnIndexOrThrow("Monster_ID"))
+        }
+        cursor.close()
+        return petID
+    }
+
+    //function to get a list of all pet monsters
+    fun getPetMonsters(ownerID: Int): ArrayList<Monster> {
+        val db = readableDatabase
+        val cursor = db.rawQuery(
+            "SELECT Name, Type, Size, Weight, Intellect, Hobby, Rarity FROM PetMonsters WHERE Owner_ID = ?",
+            arrayOf(ownerID.toString())
+        )
+        val monsters = ArrayList<Monster>()
+
+        while (cursor.moveToNext()) {
+            val monster = Monster(
+                cursor.getString(cursor.getColumnIndexOrThrow("Type")),
+                cursor.getString(cursor.getColumnIndexOrThrow("Name")),
+                cursor.getFloat(cursor.getColumnIndexOrThrow("Size")),
+                cursor.getFloat(cursor.getColumnIndexOrThrow("Weight")),
+                cursor.getInt(cursor.getColumnIndexOrThrow("Intellect")),
+                cursor.getString(cursor.getColumnIndexOrThrow("Hobby")),
+                cursor.getInt(cursor.getColumnIndexOrThrow("Rarity"))
+            )
+            monsters.add(monster)
+        }
+        cursor.close()
+        return monsters
+    }
+
+
+    //MonsterDefinitions functions:
+
+    //function to set discovered booleans according to logged in user
+    fun setDiscovered(ownerID: Int){
+        val dbwrite = writableDatabase
+        val dbread = readableDatabase
+        val cursor = dbread.rawQuery(
+            "SELECT Type_Name FROM MonsterDefinitions WHERE Owner_ID = ?",
+            arrayOf(ownerID.toString())
+        )
+        while (cursor.moveToNext()){
+            dbwrite.execSQL("UPDATE MonsterDefinitions SET Discovered = 1 WHERE Type_Name = ?",
+                arrayOf(cursor.getString(cursor.getColumnIndexOrThrow("Type_Name"))))
+        }
+
+        cursor.close()
+    }
+
+    //function to reset the monster definitions to undiscovered
+    fun resetDiscovered(){
+        val db = writableDatabase
+        db.execSQL("UPDATE MonsterDefinitions SET Discovered = 0")
+    }
+
+    //is this actually needed?
+    //function to get list of all monster types
+
+    //how to add the images to the monsters? currently saved in the drawables
+    //function to initialize monster types
+    fun initializeMonsterTypes(context: Context){
+        val db = writableDatabase
+        //common monsters
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Bonzoa", "bonzoa.png", 0, 0))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("GrayLock", "gray_lock.png", 0, 0))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("ScowlGee", "scowl_gee.png", 0, 0))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Squirmy", "squirmy.png", 0, 0))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("SnailBoo", "snail_boo.png", 0, 0))
+
+        //uncommon monsters
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Breezle", "breezle.png", 0, 1))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("KrikiToo", "kriki_too.png", 0, 1))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("PiggyWhirl", "piggy_whirl.png", 0, 1))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Rattimus", "rattimus.png", 0, 1))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Salmoosus", "salmosus.png", 0, 1))
+
+        //rare monsters
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Arrow", "arrow.png", 0, 2))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("CurlyGator", "curly_gator.png", 0, 2))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Freezy", "freezy.png", 0, 2))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("PsyCactus", "psy_cactus.png", 0, 2))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("SkelyWalker", "skely_walker.png", 0, 2))
+
+        //epic monsters
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("CheckerLord", "checker_lord.png", 0, 3))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Creepi", "creepi.png", 0, 3))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("FireFly", "fire_fly.png", 0, 3))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("FlyMousse", "fly_mousse.png", 0, 3))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Granite", "granite.png", 0, 3))
+
+
+        //legendary monsters
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Arvolok", "arvolok.png", 0, 4))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Awstrich", "awstrich.png", 0, 4))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Bramble", "bramble.png", 0, 4))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("Clastan", "clastan.png", 0, 4))
+        db.execSQL("INSERT INTO MonsterDefinitions (Type_Name, Image, Discovered, Rarity) VALUES (?,?,?,?)",
+            arrayOf("TenTwirl", "ten_twirl.png", 0, 4))
+
     }
 
     //hobbies are stored as integer values, pulls from string list of hobbies in resources
--- WanderMonsters/app/src/main/java/com/example/wandermonsters/Monster.kt
+++ WanderMonsters/app/src/main/java/com/example/wandermonsters/Monster.kt
@@ -1,11 +1,60 @@
 package com.example.wandermonsters
 
-class Monster {
-    var type = String
-    var petName = String
-    var size = Float
-    var weight = Float
-    var intellect = Int
-    var hobby = String
-    var rarity = Int
+import android.content.Context
+import android.content.res.Resources
+import java.util.Random
+
+class Monster (
+    var type: String,
+    var petName: String,
+    var size: Float,
+    var weight: Float,
+    var intellect: Int,
+    var hobby: String,
+    var rarity: Int
+){
+
+    companion object {
+
+        fun createRandomMonster(context: Context): Monster {
+            val monsterTypes = context.resources.getStringArray(R.array.monsters)
+            val hobbies = context.resources.getStringArray(R.array.hobbies)
+            val rarityType = context.resources.getStringArray(R.array.rarity)
+
+            val random = Random()
+            val randomType = random.nextInt(25)
+            val randomHobby = random.nextInt(20)
+
+            //types 1-5 are common, 6-10 are uncommon, 11-15 are rare, 16-20 are epic, 21-25 are legendary
+            var rarity = 0
+            if (randomType < 5) {
+                rarity = 0
+            } else if (randomType > 4 && randomType < 10) {
+                rarity = 1
+            } else if (randomType > 9 && randomType < 15) {
+                rarity = 2
+            } else if (randomType > 14 && randomType < 20) {
+                rarity = 3
+            } else if (randomType > 19 && randomType < 25) {
+                rarity = 4
+            }
+
+
+            val newMonster = Monster(
+                monsterTypes[randomType],
+                monsterTypes[randomType],
+                (1..100).random().toFloat(),
+                (1..100).random().toFloat(),
+                (1..10).random(),
+                hobbies[randomHobby],
+                rarity
+            )
+
+            return newMonster
+        }
+
+    }
+
+    //to call the random monster function:
+    //val newMonster = Monster.createRandomMonster(this)
 }
